46ec4c68b549cdefa46797b844250d8ac689f2ba
drewviscomi <drew.viscomi@kellogg.northwestern.edu>
2026-02-24 17:06:52 +0000

    add login/logout with BCrypt and session management

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,33 @@ class SessionsController < ApplicationController
     # render login form in sessions/new.html.erb
   end
 
-  def create
-    # authenticate the user
-    # 1. try to find the user by their unique identifier
-    # 2. if the user exists -> check if they know their password
-    # 3. if they know their password -> login is successful
-    # 4. if the user doesn't exist or they don't know their password -> login fails
-    flash["notice"] = "Welcome."
-    redirect_to "/companies"
+def create
+  # 1. Find user by email
+  @user = User.find_by(email: params["email"])
+
+  # 2 & 3. Check password using BCrypt
+  if @user != nil
+    if BCrypt::Password.new(@user["password"]) == params["password"]
+      cookies["zebra"]= "giraffe"
+      session["user_id"] = @user["id"]
+
+      flash["notice"] = "Welcome."
+      redirect_to "/companies"
+    else
+      # Wrong password
+      flash["notice"] = "Invalid email or password."
+      redirect_to "/login"
+    end
+  else
+    # 4. User doesn't exist
+    flash["notice"] = "Invalid email or password."
+    redirect_to "/login"
   end
+end
 
   def destroy
     # logout the user
+    session["user_id"] = nil
     flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,12 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+password = "goal"
+encrypted_pass = BCrypt::Password.create("goal")
+puts encrypted_pass
 # 2. prepare encrypted string for testing
+re_encrypted_pass = BCrypt::Password.new(encrypted_pass)
+puts re_encrypted_pass
 # 3. test secret against prepared encrypted string
+user_entered_password = "no_goal"
+puts re_encrypted_pass == "goal"
